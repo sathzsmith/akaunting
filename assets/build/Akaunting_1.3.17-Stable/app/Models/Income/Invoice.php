1aa25728b3657d3af1facfb5be032f6dd2548ca4
a8hok <ashokkumar.explore@gmail.com>
2020-01-15 00:34:17 +0530

    removed ic and customer_field

--- assets/build/Akaunting_1.3.17-Stable/app/Models/Income/Invoice.php
+++ assets/build/Akaunting_1.3.17-Stable/app/Models/Income/Invoice.php
@@ -33,7 +33,7 @@ class Invoice extends Model
      *
      * @var array
      */
-    protected $fillable = ['company_id', 'invoice_number', 'order_number', 'cheque_number', 'ic', 'customer_id', 'phone', 'invoice_status_code', 'invoiced_at', 'due_at', 'amount', 'currency_code', 'currency_rate', 'customer_id', 'customer_name', 'customer_email', 'customer_tax_number', 'customer_phone', 'customer_address', 'notes', 'category_id', 'parent_id'];
+    protected $fillable = ['company_id', 'invoice_number', 'order_number', 'phone', 'invoice_status_code', 'invoiced_at', 'due_at', 'amount', 'currency_code', 'currency_rate', 'customer_id', 'customer_name', 'customer_email', 'customer_tax_number', 'customer_phone', 'customer_address', 'notes', 'category_id', 'parent_id'];
 
     /**
      * Sortable columns.
@@ -50,9 +50,6 @@ class Invoice extends Model
     protected $searchableColumns = [
         'invoice_number'   => 10,
         'order_number'     => 10,
-        'cheque_number'    => 10,
-        'ic'  => 10,
-        'customer_id'      => 10,
         'phone'            => 10,
         'customer_name'    => 10,
         'customer_email'   => 5,
